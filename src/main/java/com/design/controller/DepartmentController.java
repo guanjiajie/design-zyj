47685e1f971af8ec4820594a7c47b0da7d980895
Lilicat79 <13870509754@163.com>
2020-04-13 16:29:32 +0800

    'origin'

--- src/main/java/com/design/controller/DepartmentController.java
+++ src/main/java/com/design/controller/DepartmentController.java
@@ -7,10 +7,7 @@ import com.design.entity.TDepartment;
 import com.design.service.IDepartmentService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author: guanjiajie
@@ -18,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/department")
+@CrossOrigin
 public class DepartmentController {
 
     @Autowired
